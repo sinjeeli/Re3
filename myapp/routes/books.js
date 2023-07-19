f2fdf18220f4d8dc946c10f68fc602089a568a5d
sinjeeli <sinjeeli@gmail.com>
2023-07-18 23:52:52 -0400

    we dun it

--- myapp/routes/books.js
+++ myapp/routes/books.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+// routes/books.js
+const express = require('express');
+const router = express.Router();
+const { Book } = require('../models');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+  try {
+    const books = await Book.findAll();
+    res.render('layout', { books }); // Use the 'layout.pug' template
+  } catch (error) {
+    console.error('Error fetching books:', error);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('new-book'); // Assuming you have a Pug template named 'create_book'
 });
 
 module.exports = router;
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,27 +1,32 @@
 var express = require('express');
 var router = express.Router();
-const { Book } = require('../models');
+// const { Book } = require('../models');
 
 
-router.get('/', async (req, res, next) => {
-  try {
-    const books = await Book.findAll();
-    if (books.length === 0) {
-      res.json({ message: 'No books found.' });
-    } else {
-      console.log(books);
-      res.json(books);
-    }
-  } catch (error) {
-    console.error('Error fetching books:', error);
-    res.status(500).json({ error: 'Internal server error.' });
-  }
-});
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const books = await Book.findAll();
+//     if (books.length === 0) {
+//       res.json({ message: 'No books found.' });
+//     } else {
+//       console.log(books);
+//       res.json(books);
+//     }
+//   } catch (error) {
+//     console.error('Error fetching books:', error);
+//     res.status(500).json({ error: 'Internal server error.' });
+//   }
+// });
 
 // GET /simulate-error route handler
 router.get('/simulate-error', (req, res, next) => {
   const error = new Error('Intentional 500 Error');
   next(error);
+});
+
+//
+router.get('/', (req, res, next) => {
+  res.redirect('/books');
 });
 
 
