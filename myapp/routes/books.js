603f09588988aa8dedb1cde3d47ad050fb7b830d
sinjeeli <sinjeeli@gmail.com>
2023-07-19 13:31:14 -0400

    working state

--- myapp/routes/books.js
+++ myapp/routes/books.js
@@ -14,7 +14,34 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/new', (req, res, next) => {
-  res.render('new-book'); // Assuming you have a Pug template named 'create_book'
+  res.render('new-book', { errors: null });// Assuming you have a Pug template named 'create_book'
+});
+//
+// POST /books/new - Posts a new book to the database
+router.post('/new', async (req, res, next) => {
+  try {
+    const { title, author, genre, year } = req.body;
+    
+    // Check if any required field is missing
+    if (!title || !author || !genre || !year) {
+      // If any required field is missing, render the new-book.pug template with an error message
+      return res.render('new-book', { error: 'All fields are required.' });
+    }
+
+    // Create the new book in the database
+    await Book.create({
+      title,
+      author,
+      genre,
+      year,
+    });
+
+    // Redirect the user to the home page (e.g., /books)
+    res.redirect('/books');
+  } catch (error) {
+    console.error('Error creating new book:', error);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
 });
 
 module.exports = router;
